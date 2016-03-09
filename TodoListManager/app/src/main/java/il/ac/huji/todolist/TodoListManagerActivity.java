b180c744c24f37fd74ee490f470dcb1e578b196c
alon <alonlpc@gmail.com>
2016-03-09 17:40:29 +0200

    aaaa

---
+++ TodoListManager/app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -0,0 +1,13 @@
+package il.ac.huji.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TodoListManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list_manager);
+    }
+}
