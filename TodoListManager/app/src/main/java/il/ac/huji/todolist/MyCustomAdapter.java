97be691e641978e7db86d61934a578fa26994ccf
alon <alonlpc@gmail.com>
2016-03-11 22:57:03 +0200

    submission

---
+++ TodoListManager/app/src/main/java/il/ac/huji/todolist/MyCustomAdapter.java
@@ -0,0 +1,41 @@
+package il.ac.huji.todolist;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+/**
+ * Created by user on 11/03/2016.
+ */
+public class MyCustomAdapter extends ArrayAdapter<String> {
+
+
+    public MyCustomAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+
+    public MyCustomAdapter(Context context, @LayoutRes int resource, @NonNull ArrayList<String> objects) {
+            super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView  view = (TextView)convertView;
+        if (view == null) {
+            view = (TextView)super.getView(position,convertView, parent);
+        }
+        else {
+            String item = getItem(position);
+            view.setText(item);
+        }
+        view.setTextColor(position % 2 == 0 ? Color.BLACK : Color.RED);
+        return view;
+    }
+
+}
---
+++ TodoListManager/app/src/main/java/il/ac/huji/todolist/TodoListManagerActivity.java
@@ -0,0 +1,104 @@
+package il.ac.huji.todolist;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class TodoListManagerActivity extends AppCompatActivity {
+    ArrayList<String> todoArray =new ArrayList<String>();
+    private String m_Text = "";
+    AlertDialog.Builder builder;
+    MyCustomAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list_manager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        for(int i = 0; i < 20; i++) {
+            todoArray.add("test task " + Integer.toString(i));
+        }
+        adapter = new MyCustomAdapter(this, R.layout.support_simple_spinner_dropdown_item, todoArray);
+
+        ListView listView = (ListView) findViewById(R.id.todo_list);
+        listView.setAdapter(adapter);
+
+        builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add a task");
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @Override
+            public boolean onItemLongClick(final AdapterView<?> parent, View view, final int position, long id) {
+
+                final AlertDialog.Builder b = new AlertDialog.Builder(TodoListManagerActivity.this);
+                b.setIcon(android.R.drawable.ic_dialog_alert);
+                b.setMessage(((TextView)view).getText().toString());
+                b.setNeutralButton("Delete " + ((TextView) view).getText().toString(), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        adapter.remove(adapter.getItem(position));
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+                b.show();
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_todo_list_manager, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+// Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                m_Text = input.getText().toString();
+                synchronized (adapter) {
+                    todoArray.add(m_Text);
+                    adapter.notify();
+                }
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+        return true;
+
+    }
+}
